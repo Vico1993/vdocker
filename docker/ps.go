1de502463d70480fb12e188b72a018d00eee628f
Victor Piolin <victor.piolin@gmail.com>
2019-05-29 23:03:12 -0700

    Docker PS first versions

--- docker/ps.go
+++ docker/ps.go
@@ -1,8 +1,13 @@
 package docker
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os/exec"
+	"strings"
 )
 
 // Dockerps is the struct to handle docker ps output
@@ -23,4 +28,35 @@ func (d *Dockerps) UnmarshalJSON(buf []byte) error {
 		return fmt.Errorf("wrong number of fields in DockerPS: %d != %d", g, e)
 	}
 	return nil
+}
+
+// Ps return the docker ps
+func Ps() ([]Dockerps, error) {
+	var res []Dockerps
+
+	out, err := exec.Command("docker", "ps", "--format", "['{{.Image}}','{{.Names}}','{{.Status}}']").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	r := bufio.NewReader(bytes.NewReader(out))
+	for i := 1; ; i++ {
+		line, _, err := r.ReadLine()
+		if err != nil && err == io.EOF {
+			return res, nil
+		}
+
+		str := string(line[:])
+		// first line exeption
+		if strings.Contains(str, "CONTAINER ID") {
+			continue
+		}
+
+		var d Dockerps
+		if e := json.Unmarshal([]byte(strings.ReplaceAll(str, "'", "\"")), &d); e != nil {
+			return nil, err
+		}
+
+		res = append(res, d)
+	}
 }
--- main.go
+++ main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
+	"fmt"
 	"log"
-	"os/exec"
-	"strings"
 
 	"github.com/Vico1993/vdocker/docker"
 )
 
 func main() {
-	out, err := exec.Command("docker", "ps", "--format", "['{{.Image}}','{{.Names}}','{{.Status}}']").Output()
+	dockerList, err := docker.Ps()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf(err.Error())
 	}
 
-	r := bufio.NewReader(bytes.NewReader(out))
-	for i := 1; ; i++ {
-		line, _, err := r.ReadLine()
-		if err != nil {
-			break
-		}
-
-		str := string(line[:])
-		// first line exeption
-		if strings.Contains(str, "CONTAINER ID") {
-			continue
-		}
-
-		var d docker.Dockerps
-		if err := json.Unmarshal([]byte(strings.ReplaceAll(str, "'", "\"")), &d); err != nil {
-			log.Fatal(err)
-		}
+	for _, d := range dockerList {
+		fmt.Println(d)
 	}
 }
